e53b4f441c1df7619d3b644d2e4384a8038b9827
llutti <llutti@outlook.com>
2021-04-20 11:42:10 -0300

    Create the webpack config and add dependencies

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const path = require('path');
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+module.exports = {
+	target: 'node',
+	node: {
+		__dirname: false,
+	},
+	mode: 'production',
+
+	entry: {
+		extension: './client/src/extension.ts',
+		server: './server/out/server.js'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		libraryTarget: 'commonjs2',
+		devtoolModuleFilenameTemplate: '../[resource-path]'
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: 'commonjs vscode'
+	},
+	resolve: {
+		extensions: ['.ts', '.js', 'json']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.node$/,
+				loader: 'node-loader',
+			},
+		]
+	}
+};
